184a39dab3a5401a57b2746248b149b67d745c81
Harsh Sachin Chandekar <59416466+harshchan@users.noreply.github.com>
2021-03-20 00:49:09 +0530

    Create bus routes.cpp

---
+++ bus routes.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
+
+int busday()
+{
+	lli n,d,i,l,r;
+	 lli A[100];
+	//vector<int>B;
+	cin>>n>>d;
+	for(i=0;i<n;i++)
+	{
+		cin>>A[i];
+	}
+	
+	if(n=1)
+	{
+		return 1;
+	}
+	for(i=0;i<n-1;i++)
+	{
+		l=(d/A[0])*A[0];
+		r=(d/A[n-i-1])*A[n-i-1];
+		
+		while(l>=r)
+		{
+			l=l-A[0];
+		}
+	}
+	return l;
+}
+
+int main()
+{
+    int t,x,n=1;
+    cin>>t;
+    
+    while(n<=t)
+    {
+        x=busday();
+        cout<<"Case #"<<n<<": "<<x<<endl;
+        n++;
+    }
+    return 0;
+}
